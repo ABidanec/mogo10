c6ec2886899d2fa1375607bc62ad2ab7b529566a
Alexandr Bidanec <a.bidanec@ukr.net>
2016-11-30 20:11:56 +0200

    Н работающий плагин masonry для блока works

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".b-services2__list-desc").niceScroll({cursorcolor:"#95e1d3"});
+    moduleMasonry.action();
 });
-
 
 // module view block
 /**
@@ -14,4 +14,35 @@ var moduleViewBlock = (function(){
         elemTextList = elemText.parent("ul"),
         elemImg = $(".b-services2__item-img"),
         elemImgList = elemImg.parent("ul");
+})();
+
+
+
+
+
+/**
+moduleMasonry - модуль адаптивных плиток
+*/
+var moduleMasonry = (function(){
+    var fields = {
+        container : $(".b-works__list"),
+        element : $(".b-works__item")
+    }
+    
+    var _actionMasonry = function(){
+        fields.container.imagesLoaded(function(){
+            fields.container.masonry({
+                itemSelector : fields.element,
+                columnWidth : fields.element,
+                percentPosition: true
+            });
+        });
+        
+    };
+    
+    return {
+        action : function(){
+            _actionMasonry();
+        }
+    }
 })();
